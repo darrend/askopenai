23d758851a92ed7bbb76aa1de571ed23c8a3e3b5
Darren Day <darrenbday@gmail.com>
2023-01-29 19:24:25 +0000

    playing around and at a good spot

---
+++ main.ts
@@ -0,0 +1,14 @@
+import { OpenAI } from "https://deno.land/x/openai_deno@v0.9.1/mod.ts";
+
+const instance = new OpenAI();
+
+const response = await instance.createCompletion("text-davinci-003", {
+  prompt: `${Deno.args[0]}:\n\n\n`,
+  maxTokens: 1024,
+  temperature: 0,
+  count: 1,
+});
+
+response.choices.forEach((choice) => {
+  console.info(choice.text);
+});
